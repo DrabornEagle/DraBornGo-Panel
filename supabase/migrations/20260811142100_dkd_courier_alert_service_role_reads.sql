26111eb05a5579a95601ca5b41532927e2a15aee
DrabornEagle <draborneagle@gmail.com>
2026-08-11 14:27:23 +0300

    chore: track courier alert service role read grants

---
+++ supabase/migrations/20260811142100_dkd_courier_alert_service_role_reads.sql
@@ -0,0 +1,4 @@
+grant select on table public.dkd_businesses to service_role;
+grant select on table public.dkd_business_couriers to service_role;
+grant select on table public.dkd_push_tokens to service_role;
+grant select on table public.dkd_courier_jobs to service_role;
